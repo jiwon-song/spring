508d39ce12ef5a768fc37948a9494640e5e8fd84
Honggihun1 <103994463+Honggihun1@users.noreply.github.com>
2022-08-04 21:38:10 +0900

    테스트 추가 , 상세 기능 구현

--- Project_Spring/project/src/main/java/hong/HongService.java
+++ Project_Spring/project/src/main/java/hong/HongService.java
@@ -11,13 +11,13 @@ public class HongService {
 	@Autowired private HongDAO dao;
 
 	// 반목록 가져오기
-	public List<ClassListVO> class_list() {
-		return dao.class_list();		
+	public List<ClassListVO> class_list(String teacher_id) {
+		return dao.class_list(teacher_id);		
 	}
 
 	// 테스트 목록 가져오기
-	public List<TestVO> test_list(int class_id) {	
-		return dao.test_list(class_id);
+	public List<TestVO> test_list(int class_id, String teacher_id) {	
+		return dao.test_list(class_id, teacher_id);
 	}
 
 	// 테스트 추가하기
@@ -29,6 +29,14 @@ public class HongService {
 	public void test_delete(int test_id) {
 		dao.test_delete(test_id);
 		
+	}
+
+	public void test_update(TestVO vo) {
+		dao.test_update(vo);
+	}
+
+	public List<TestDetailVO> test_detail(int test_id) {
+		return dao.test_detail(test_id);
 	}
 
 	
